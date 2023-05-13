81c41d4a6ae46d207dfcf020b0ce57a1b3be818a
Rebeca <rehdomingos123@gmail.com>
2023-05-13 19:33:29 -0300

    add db connect, cors and routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import dbConnect from './config/dbConnect.js';
+import colaboratorRouter from './routes/colaboratorRoute.js';
+import cors from 'cors';
+
+dbConnect();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+colaboratorRouter(app);
+
 
 export default app;
